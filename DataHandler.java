2c3e34ba5c4e515a7143abb566e475183d7ead72
AlyS1991 <114189928+AlyS1991@users.noreply.github.com>
2022-11-28 22:14:21 +0000

    Adding code. Using sample code from Lab7.

---
+++ DataHandler.java
@@ -0,0 +1,133 @@
+/*
+    
+ */
+//package gui_tutorial;
+
+//Sample code from Lab 7 for connecting to the database file.
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DataHandler {
+// DB details
+
+    private static final String dbURL = "jdbc:ucanaccess://librarydb.accdb;sysSchema=true";
+    private static java.sql.Connection con;
+    private static java.sql.Statement stm;
+    private static java.sql.ResultSet rs;
+    private static java.sql.ResultSetMetaData rsMeta;
+    private static int columnCount;
+
+    public static Vector<String> getTables() {
+        Vector<String> l = new Vector<>();
+        /*l.add("Members");
+        l.add("Items");
+        l.add("Locations");*/
+        String sqlQuery = "SELECT Name FROM sys.MSysObjects WHERE Type=1 AND Flags=0";
+        try {
+            Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+            con = DriverManager.getConnection(dbURL, "", "");
+            stm = con.createStatement();
+            rs = stm.executeQuery(sqlQuery);
+            while (rs.next()) {
+            // each row is an array of objects
+                    l.add((String) rs.getObject(1));
+            }
+        } catch (ClassNotFoundException cnfex) {
+            System.err.println("Issue with the JDBC driver.");
+            System.exit(1); // terminate program - cannot recover
+        } catch (java.sql.SQLException sqlex) {
+            System.err.println(sqlex);
+        } catch (Exception ex) {
+            System.err.println(ex);
+            //ex.printStackTrace();
+        } finally {
+            try {
+                if (null != con) {
+                    // cleanup resources, once after processing
+                    rs.close();
+                    stm.close();
+                    // and then finally close connection
+                    con.close();
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(DataHandler.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return l;
+    }
+
+    public static void searchRecords(String table) {
+        String sqlQuery = "SELECT * FROM " + table;
+        try {
+            Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+            con = DriverManager.getConnection(dbURL, "", "");
+            stm = con.createStatement(
+                    java.sql.ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    java.sql.ResultSet.CONCUR_READ_ONLY);
+            rs = stm.executeQuery(sqlQuery);
+            rsMeta = rs.getMetaData();
+            columnCount = rsMeta.getColumnCount();
+        } catch (ClassNotFoundException cnfex) {
+            System.err.println("Issue with the JDBC driver.");
+            System.exit(1); // terminate program - cannot recover
+        } catch (java.sql.SQLException sqlex) {
+            System.err.println(sqlex);
+        } catch (Exception ex) {
+            System.err.println(ex);
+            //ex.printStackTrace();
+        }
+    }
+
+    public static Object[] getTitles(String table) {
+        Object[] columnNames = new Object[columnCount];
+        try {
+            for (int col = columnCount; col > 0; col--) {
+                columnNames[col - 1]
+                        = rsMeta.getColumnName(col);
+            }
+        } catch (java.sql.SQLException sqlex) {
+            System.err.println(sqlex);
+        } finally {
+            try {
+                if (null != con) {
+                    // cleanup resources, once after processing
+                    rs.close();
+                    stm.close();
+                    // and then finally close connection
+                    con.close();
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(DataHandler.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return columnNames;
+    }
+
+    public static Object[][] getRows(String table) {
+        searchRecords(table);
+        Object[][] content;
+        try {
+// determine the number of rows
+            rs.last();
+            int number = rs.getRow();
+            content = new Object[number][columnCount];
+            rs.beforeFirst();
+            int i = 0;
+            while (rs.next()) {
+// each row is an array of objects
+                for (int col = 1; col <= columnCount; col++) {
+                    content[i][col - 1] = rs.getObject(col);
+                }
+                i++;
+            }
+            return content;
+        } catch (java.sql.SQLException sqlex) {
+            System.err.println(sqlex);
+        }
+        return null;
+    }
+}
